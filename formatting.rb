5751649a84a030ae2161f6238e1e4d2a80ddd1dd
Sara Habecker <shabecker@gannett.com>
2021-11-29 11:44:10 -0500

    format

--- formatting.rb
+++ formatting.rb
@@ -56,4 +56,25 @@ p s2.chomp
 
 
 
+s = "now is the winter of our discontent
+Made glorious summer by this sun of York;
+And all the clouds that   lour'd upon our house
+In the deep bosom of the ocean buried."
 
+puts('--- s.length ---')
+puts(s.length)
+
+puts('--- s.reverse ---')
+puts(s.reverse)
+
+puts('--- s.upcase ---')
+puts(s.upcase)
+
+puts('--- s.capitalize ---')
+puts(s.capitalize)
+
+puts('--- s.swapcase ---')
+puts(s.swapcase)
+
+puts('--- s.downcase ---')
+puts(s.downcase)
