499816c7949f2c3c4b37da7641b055a5cf472671
Sara Habecker <shabecker@gannett.com>
2021-11-29 11:19:43 -0500

    strings

---
+++ 1strings.rb
@@ -0,0 +1,12 @@
+class MyClass
+  def timesten(aNumber)
+    return aNumber * 10
+  end
+end
+
+ob = MyClass.new
+#below does not work because it has single quotes
+puts('single quotes: #{ob.timesten(5)}')
+
+#below works because it's in double quotes
+puts("double quotes: #{ob.timesten(5)}")
